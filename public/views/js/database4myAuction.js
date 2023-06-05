0dacc0c0a93592b944f8450e6df182e63a915eb5
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2023-06-05 19:12:16 +0530

    liker players.ejs

--- public/views/js/database4myAuction.js
+++ public/views/js/database4myAuction.js
@@ -1,3 +1,4 @@
+let count = 0;
 const socket = io('https://auction.cyclic.app');
 
 const search = document.getElementById("change");
@@ -31,8 +32,8 @@ function decreaseOrder() {
 
 function changePlayer(playerOrder) {
 
-    socket.emit('change-Player',playerOrder);
-console.log("emiited");
+    socket.emit('change-Player', playerOrder);
+    console.log("emiited");
     //set bids section to default
     document.getElementById("hidden").style.display = "none";
     document.getElementById("bids-3").style.display = "block";
@@ -42,14 +43,14 @@ console.log("emiited");
 }
 
 
-socket.on('change-Player',(result)=>{
+socket.on('change-Player', (result) => {
     console.log(result);
     result.forEach(myElement => {
         document.getElementById("name").innerHTML = myElement.name;
         document.getElementById("playerNum").innerHTML = myElement.order;
-        document.getElementById("currPlayerImg").src = "/resources/Players/" + myElement.name+".png";
-        document.getElementById("roleImg").src = "/resources/icons/" + myElement.Role+".png";
-        document.getElementById("flagImg").src = "/resources/icons/" + myElement.Nationality+".png";
+        document.getElementById("currPlayerImg").src = "/resources/Players/" + myElement.name + ".png";
+        document.getElementById("roleImg").src = "/resources/icons/" + myElement.Role + ".png";
+        document.getElementById("flagImg").src = "/resources/icons/" + myElement.Nationality + ".png";
         document.getElementById("batAvg").innerHTML = myElement.batPts;
         document.getElementById("bowlPts").innerHTML = myElement.bowlPts;
         document.getElementById("basePrice").innerHTML = myElement.BasePrice;
@@ -66,3 +67,70 @@ socket.on('change-Player',(result)=>{
     });
 })
 
+
+
+
+
+
+
+if (document.getElementById("searchOrder").innerHTML = "woww" && count > 0) {
+    search.addEventListener("click", decideOrder);
+    next.addEventListener("click", increaseOrder);
+    prev.addEventListener("click", decreaseOrder);
+
+    function decideOrder() {
+        let order = input.value;
+        changePlayer(order)
+    }
+
+    function increaseOrder() {
+        let order = current_player_order.innerText;
+        order++;
+        console.log(playerNumber);
+        changePlayer(playerNumber + "");
+    }
+    function decreaseOrder() {
+        let order = current_player_order.innerHTML;
+        order--;
+        console.log(playerNumber);
+        changePlayer(playerNumber + "");
+    }
+
+    function changePlayer(playerOrder) {
+
+        socket.emit('change-Player', playerOrder);
+        console.log("emiited");
+        //set bids section to default
+        document.getElementById("hidden").style.display = "none";
+        document.getElementById("bids-3").style.display = "block";
+        document.getElementById("UP").style.display = "block";
+        document.getElementById("Sold").style.display = "block";
+        document.getElementById("teamSelector").style.display = "none";
+    }
+
+
+    socket.on('change-Player', (result) => {
+        console.log(result);
+        result.forEach(myElement => {
+            document.getElementById("name").innerHTML = myElement.name;
+            document.getElementById("playerNum").innerHTML = myElement.order;
+            document.getElementById("currPlayerImg").src = "/resources/Players/" + myElement.name + ".png";
+            document.getElementById("roleImg").src = "/resources/icons/" + myElement.Role + ".png";
+            document.getElementById("flagImg").src = "/resources/icons/" + myElement.Nationality + ".png";
+            document.getElementById("batAvg").innerHTML = myElement.batPts;
+            document.getElementById("bowlPts").innerHTML = myElement.bowlPts;
+            document.getElementById("basePrice").innerHTML = myElement.BasePrice;
+            document.getElementById("wkPts").innerHTML = myElement.wkPts;
+            document.getElementById("nationality").innerHTML = myElement.Nationality;
+            document.getElementById("role").innerHTML = myElement.Role;
+
+            //set bids section to default
+            document.getElementById("hidden").style.display = "none";
+            document.getElementById("bids-3").style.display = "block";
+            document.getElementById("UP").style.display = "block";
+            document.getElementById("Sold").style.display = "block";
+            document.getElementById("teamSelector").style.display = "none";
+        });
+    })
+
+}
